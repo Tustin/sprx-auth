5d92b8d7eb33c6318f18baa80ac4ab0dc1741383
AlgebraNGU <brazilderek78@gmail.com>
2017-12-15 13:50:43 +0000

    Update setup.php
    Put the Primary Key (id) in the first column maybe for the users readability? Like the query on line 41 it just makes it look nicer.

--- setup.php
+++ setup.php
@@ -3,30 +3,29 @@
 include('autoload.php');
 
 if (empty($db_user) || empty($db_password))
-{
   die("Missing MySQL user credentials. Please specify a username and password inside 'autoload.php' before running the setup.");
-}
+
 try {
-$setup = new PDO("mysql:host=".$db_host, $db_user, $db_password);
+  $setup = new PDO("mysql:host=".$db_host, $db_user, $db_password);
 } 
 catch (PDOException $e) {
     die("An error occurred when logging into MySQL - " . $e->getMessage());
 }
+
 echo '<pre>';
 
 $query = $setup->prepare("CREATE DATABASE " . DATABASE_NAME);
 
-if ($query->execute()){
-echo "Created database '" . DATABASE_NAME ."' <font color='green'>successfully</font><br>";
-} else {
+if ($query->execute())
+  echo "Created database '" . DATABASE_NAME ."' <font color='green'>successfully</font><br>";
+else
   echo "<font color='red'>ERROR</font> occurred when creating database '" . DATABASE_NAME . "' - " . $query->errorCode() . " (maybe MySQL user doesn't have sufficent permissions?)<br>";
-}
 
 $query = $setup->prepare("CREATE TABLE " . DATABASE_NAME . ".users (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `license` text NOT NULL,
   `ip` text NOT NULL,
   `dateline` bigint(20) NOT NULL,
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `times_banned` int(11) NOT NULL,
   `banned` tinyint(4) NOT NULL,
   `ban_reason` text NOT NULL,
@@ -34,11 +33,10 @@ $query = $setup->prepare("CREATE TABLE " . DATABASE_NAME . ".users (
   `unban_dateline` int(11) NOT NULL
 )");
 
-if ($query->execute()){
+if ($query->execute())
   echo "Created table 'users' in database '" . DATABASE_NAME . "' <font color='green'>successfully</font><br>";
-} else {
+else
   echo "<font color='red'>ERROR</font> occurred when creating table 'users' - " . $query->errorCode() . "<br>";
-}
 
 $query = $setup->prepare("CREATE TABLE " . DATABASE_NAME . ".log (
   `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -47,11 +45,10 @@ $query = $setup->prepare("CREATE TABLE " . DATABASE_NAME . ".log (
   `dateline` bigint(20) NOT NULL
 )");
 
-if ($query->execute()){
+if ($query->execute())
   echo "Created table 'log' in database '" . DATABASE_NAME . "' <font color='green'>successfully</font><br>";
-} else {
+else
   echo "<font color='red'>ERROR</font> occurred when creating table 'log' - " . $query->errorCode() . "<br>";
-}
 
 echo "Completed operations. <br>";
 
